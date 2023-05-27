c167f3e89efdb3a19e8e0137d68c15dac006314e
Jean <jean@sherpaerp.com>
2023-05-27 18:25:28 -0300

    create repository

---
+++ src/main/java/com/jb/polling_station/repository/MeetingAgendaRepository.java
@@ -0,0 +1,11 @@
+package com.jb.polling_station.repository;
+
+import com.jb.polling_station.entity.MeetingAgenda;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MeetingAgendaRepository extends BaseRepository<MeetingAgenda> {
+
+    
+}
---
+++ src/main/java/com/jb/polling_station/repository/PollRepository.java
@@ -0,0 +1,12 @@
+package com.jb.polling_station.repository;
+
+import com.jb.polling_station.entity.Poll;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PollRepository extends BaseRepository<Poll> {
+    
+    
+
+}
