52af8d62a304a8214ac904c2edafee6d9ed3224e
Jean <jean@sherpaerp.com>
2023-05-27 18:28:58 -0300

    draft meeting agenda service

---
+++ src/main/java/com/jb/polling_station/service/MeetingAgendaService.java
@@ -0,0 +1,26 @@
+package com.jb.polling_station.service;
+
+import com.jb.polling_station.entity.MeetingAgenda;
+import com.jb.polling_station.record.MeetingAgendaRequestDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MeetingAgendaService {
+    public void delete(int id) {
+    
+    }
+    
+    public List<MeetingAgenda> getAll() {
+        return null;
+    }
+    
+    public MeetingAgenda getListById(int id) {
+        return null;
+    }
+    
+    public MeetingAgenda createList(MeetingAgendaRequestDTO requestDTO) {
+        return null;
+    }
+}
