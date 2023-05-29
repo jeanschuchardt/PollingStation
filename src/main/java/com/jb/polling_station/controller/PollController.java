ab92f2d0859cc26316d58d9ddadd62a02607cb35
Jean <jean@sherpaerp.com>
2023-05-29 19:40:11 -0300

    votes

--- src/main/java/com/jb/polling_station/controller/PollController.java
+++ src/main/java/com/jb/polling_station/controller/PollController.java
@@ -1,9 +1,12 @@
 package com.jb.polling_station.controller;
 
 
+import com.jb.polling_station.entity.SessionPoll;
 import com.jb.polling_station.record.PollSessionRequestDTO;
+import com.jb.polling_station.record.SessionResult;
 import com.jb.polling_station.record.VoteRequestDTO;
 import com.jb.polling_station.service.PollService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +21,8 @@ public class PollController {
     }
     
     @PostMapping("/api/v1/poll")
-    public void createPollSession(PollSessionRequestDTO pollSessionRequestDTO) {
-        pollService.createNewPollSession(pollSessionRequestDTO);
+    public SessionPoll createPollSession(PollSessionRequestDTO pollSessionRequestDTO) {
+       return  pollService.createNewPollSession(pollSessionRequestDTO);
     }
     
     
@@ -27,6 +30,12 @@ public class PollController {
     public void vote(Long agendaId, Long sessionId, VoteRequestDTO voteRequestDTO) {
         pollService.vote(agendaId,sessionId,voteRequestDTO);
     }
+    
+    @GetMapping("/api/v1/agenda/{agendaId}/poll/{sessionId}")
+    public SessionResult getResult(Long agendaId, Long sessionId) {
+        return pollService.getResults(agendaId, sessionId);
+    }
+    
     
     
 }
